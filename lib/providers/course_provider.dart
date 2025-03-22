49f085750cd51ac6bb4476264095158740581107
Shafi <jonyszone@gmail.com>
2025-03-22 11:08:04 +0600

    course provider implementation as viewmodel

---
+++ lib/providers/course_provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../models/course_model.dart';
+import '../services/course_service.dart';
+
+class CourseProvider with ChangeNotifier {
+  final CourseService _courseService = CourseService();
+  List<Course> _courses = [];
+  List<Course> _filteredCourses = [];
+  bool _isLoading = false;
+
+  List<Course> get filteredCourses => _filteredCourses;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadCourses() async {
+    _isLoading = true;
+    notifyListeners();
+
+    _courses = await _courseService.loadCourses();
+    _filteredCourses = _courses;
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void searchCourses(String query) {
+    if (query.isEmpty) {
+      _filteredCourses = _courses;
+    } else {
+      _filteredCourses = _courses
+          .where((course) =>
+      course.courseName.toLowerCase().contains(query.toLowerCase()) ||
+          course.universityName.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+    notifyListeners();
+  }
+}
